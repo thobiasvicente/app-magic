9722cfb567c7b96940645587dfd16d048f70a7f4
Daniel Reis <daniel.reis@zemis.com.br>
2020-01-24 12:02:24 -0300

    adicionado axios e feita primeira chamda api scryfall

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,20 +1,33 @@
 <template>
   <q-page padding class="flex flex-center">
-    <q-input v-model="text" :dense="dense" label="Search Cards">
+    <q-input v-model="name" label="Search Cards" v-on:keyup.enter="submit(name)">
       <template v-slot:append>
-        <q-icon name="search" />
+        <q-icon name="search" v-on:click="submit(name)"/>
       </template>
     </q-input>
+      <p>{{card}}</p>  
   </q-page>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data() {
     return {
-      text: "",
-      ph: ""
-    };
-  }
+      card: '',
+      name: ''
+    }
+   
+  },
+
+  methods: {
+    submit (name) {
+      axios.get(`https://api.scryfall.com/cards/named?fuzzy=${name}`)
+      .then(response => (this.card = response.data))
+      .catch(err => (err))
+    },
+   
+  },
+
 };
 </script>
