6fbefeaa52d65153872a82f4eb3cd5bbad339c93
thobiasvicente <thobiascontact@gmail.com>
2020-01-25 00:27:45 -0300

    just removing comments

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -19,7 +19,6 @@ export default {
         .auth()
         .signInWithPopup(provider)
         .then(result => {
-          // This gives you a Google Access Token. You can use it to access the Google API.
           let token = result.credential.accessToken;
           // The signed-in user info.
           let user = result.user;
